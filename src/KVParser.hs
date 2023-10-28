8595263548385eb6e4392fc84045fef64616a9dc
HAZELNUT <stran19@student.aau.dk>
2023-10-28 23:54:19 +0200

    add indentation

--- src/KVParser.hs
+++ src/KVParser.hs
@@ -12,12 +12,43 @@ import Text.Parsec.Language (LanguageDef, emptyDef)
 import Text.Parsec.String (Parser)
 import qualified Text.Parsec.Token as P
 
-data Root = Root String Nodes deriving (Show)
-type Nodes = [Node]
-data Node = KVNode KVPair | NestedRoot Root deriving (Show)
+data Root = Root String Nodes NestingLevel
+
+instance Show Root where
+  show (Root key elems nestingLevel) =
+    whitespaces nestingLevel
+      ++ show key
+      ++ "\n"
+      ++ whitespaces nestingLevel
+      ++ "{\n"
+      ++ show elems
+      ++ "\n"
+      ++ whitespaces nestingLevel
+      ++ "}\n"
+
+data Nodes = Nodes [Node] NestingLevel
+
+instance Show Nodes where
+  show (Nodes (x : xs) nestingLevel) =
+    show x ++ show (Nodes xs nestingLevel)
+  show (Nodes [] _) = ""
+
+data Node = KVNode KVPair NestingLevel | NestedRoot Root NestingLevel
+
+instance Show Node where
+  show (KVNode (x, y) nestingLevel) =
+    whitespaces nestingLevel ++ show x ++ "    " ++ show y ++ "\n"
+  show (NestedRoot root nestingLevel) = show root
 
 type KVPair = (String, String)
 type KVFile = Root
+type NestingLevel = Int
+
+whitespaces :: NestingLevel -> String
+whitespaces n = replicate (n * 2) ' '
+
+doubleQuotes :: String
+doubleQuotes = "\""
 
 languageDEF :: P.LanguageDef st
 languageDEF =
@@ -44,32 +75,42 @@ parseKey = do
 
   return key
 
-parseKVPair :: Parser Node
-parseKVPair = do
+parseKVPair :: NestingLevel -> Parser Node
+parseKVPair nestingLevel = do
   key <- parseKey
   value <- parseKey
-  return $ KVNode (key, value)
+  return $ KVNode (key, value) nestingLevel
 
-parseNestedLeaf :: Parser Node
-parseNestedLeaf = NestedRoot <$> parseNestedValue
+parseNestedLeaf :: NestingLevel -> Parser Node
+-- parseNestedLeaf = NestedRoot <$> parseNestedValue
+parseNestedLeaf nestingLevel = do
+  nested <- parseNestedValue nestingLevel
+  return $ NestedRoot nested nestingLevel
 
-parseNodes :: Parser Nodes
-parseNodes = many (try parseKVPair <|> try parseNestedLeaf)
+parseNodes :: NestingLevel -> Parser Nodes
+-- parseNodes nestingLevel = Nodes <$> many (try parseKVPair <|> try parseNestedLeaf)
+parseNodes nestingLevel = do
+  nodes <- many (try (parseKVPair nestingLevel) <|> try (parseNestedLeaf nestingLevel))
+  return $ Nodes nodes nestingLevel
 
-parseNestedValue :: Parser Root
-parseNestedValue = do
+parseNestedValue :: NestingLevel -> Parser Root
+parseNestedValue nestingLevel = do
   key <- parseKey
   parseLexeme "{"
-  nodes <- parseNodes
+  nodes <- parseNodes $ nestingLevel + 1
   parseLexeme "}"
-  return $ Root key nodes
+  return $ Root key nodes nestingLevel
+
+-- getNesting :: Node -> Int
+-- getNesting (KVNode _ n) = n
+-- getNesting (NestedRoot _ n) = n
 
 parseKVFile :: String -> IO (Either ParseError KVFile)
 parseKVFile filename = do
   handle <- openFile filename ReadMode
   hSetEncoding handle utf8
   contents <- hGetContents handle
-  case parse parseNestedValue "" contents of
+  case parse (parseNestedValue 0) "" contents of
     Left err -> do
       let pos = errorPos err
       putStrLn $ "Parse error at " ++ (show . sourceLine . errorPos $ err) ++ ":" ++ (show . sourceColumn . errorPos $ err)
